03b7ee664416381105e0313203ee8a3aee98d170
jacver <jacobvernau@gmail.com>
2022-03-05 13:57:50 -0700

    win condition implemented, needs to be refactored

--- script.js
+++ script.js
@@ -37,10 +37,22 @@ const squares = document.querySelectorAll(".square");
 
 const displayActivePlayer = document.querySelector(".active-player");
 
+const btnReset = document.querySelector(".btn-reset");
+
+const square1 = document.querySelector(".one");
+const square2 = document.querySelector(".two");
+const square3 = document.querySelector(".three");
+const square4 = document.querySelector(".four");
+const square5 = document.querySelector(".five");
+const square6 = document.querySelector(".six");
+const square7 = document.querySelector(".seven");
+const square8 = document.querySelector(".eight");
+const square9 = document.querySelector(".nine");
+
 let activePlayer = 1;
 let turn = 1;
-
-const btnReset = document.querySelector(".btn-reset");
+let playerOneWins = 0;
+let player2Wins = 0;
 
 // =============================
 // ======Event Listeners========
@@ -62,9 +74,13 @@ btnReset.addEventListener("click", gameInit);
 
 function gameInit() {
   squares.forEach((square) => {
+    // clear out buttons and re-enable them
     square.innerText = "";
     square.disabled = false;
+    // set active player to 1 in game and header
     activePlayer = 1;
+    displayActivePlayer.innerText = `${activePlayer}`;
+    // set turn to 1
     turn = 1;
   });
 }
@@ -86,8 +102,73 @@ function runTurn(square) {
   turn++;
   displayActivePlayer.innerText = `${activePlayer}`;
   // CHECK WIN CONDITION
-  //   if (turn === 10) {
-  //     console.log(`Game over - turn ${turn}`);
-  //     turn = 1;
-  //   }
+  win();
+}
+
+function win() {
+  if (
+    (square1.innerText === "X" &&
+      square2.innerText === "X" &&
+      square3.innerText === "X") ||
+    (square7.innerText === "X" &&
+      square8.innerText === "X" &&
+      square9.innerText === "X") ||
+    (square4.innerText === "X" &&
+      square5.innerText === "X" &&
+      square6.innerText === "X") ||
+    (square1.innerText === "X" &&
+      square4.innerText === "X" &&
+      square7.innerText === "X") ||
+    (square2.innerText === "X" &&
+      square5.innerText === "X" &&
+      square8.innerText === "X") ||
+    (square3.innerText === "X" &&
+      square6.innerText === "X" &&
+      square9.innerText === "X") ||
+    (square1.innerText === "X" &&
+      square5.innerText === "X" &&
+      square9.innerText === "X") ||
+    (square3.innerText === "X" &&
+      square5.innerText === "X" &&
+      square7.innerText === "X")
+  ) {
+    console.log("win");
+    playerOneWins++;
+    alert("Player 1 Wins!");
+    document.querySelector(
+      ".player1"
+    ).innerText = `Player 1 Wins = ${playerOneWins}`;
+  } else if (
+    (square1.innerText === "O" &&
+      square2.innerText === "O" &&
+      square3.innerText === "O") ||
+    (square7.innerText === "O" &&
+      square8.innerText === "O" &&
+      square9.innerText === "O") ||
+    (square4.innerText === "O" &&
+      square5.innerText === "O" &&
+      square6.innerText === "O") ||
+    (square1.innerText === "O" &&
+      square4.innerText === "O" &&
+      square7.innerText === "O") ||
+    (square2.innerText === "O" &&
+      square5.innerText === "O" &&
+      square8.innerText === "O") ||
+    (square3.innerText === "O" &&
+      square6.innerText === "O" &&
+      square9.innerText === "O") ||
+    (square1.innerText === "O" &&
+      square5.innerText === "O" &&
+      square9.innerText === "O") ||
+    (square3.innerText === "O" &&
+      square5.innerText === "O" &&
+      square7.innerText === "O")
+  ) {
+    console.log("win");
+    player2Wins++;
+    alert("Player 2 Wins!");
+    document.querySelector(
+      ".player2"
+    ).innerText = `Player 2 Wins = ${player2Wins}`;
+  }
 }
